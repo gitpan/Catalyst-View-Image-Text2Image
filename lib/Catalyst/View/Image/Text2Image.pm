d35ab643be496e88af60bc7ce85094f202d451fb
Martin Gillmaier <gillmaus@cpan.org>
2012-03-14 19:18:49 +0000

    Import of GILLMAUS/Catalyst-View-Image-Text2Image-0.02 from CPAN.
    gitpan-cpan-distribution: Catalyst-View-Image-Text2Image
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         GILLMAUS/Catalyst-View-Image-Text2Image-0.02.tar.gz
    gitpan-cpan-author:       GILLMAUS
    gitpan-cpan-maturity:     released

--- lib/Catalyst/View/Image/Text2Image.pm
+++ lib/Catalyst/View/Image/Text2Image.pm
@@ -7,12 +7,14 @@ use GD::Simple;
 
 =head1 NAME
 
-Catalyst::View::Image::Text2Image - Catalyst view to create text into image using GD::Simple;
+Catalyst::View::Image::Text2Image - View to create text into image AND render GD::Simple-images;
 
 =head1 DESCRIPTION
 
 Catalyst::View::Image::Text2Image is a view that creates images using GD::Simple.
 You can set $c->stash->{Text2Image} to several options that will define your output.
+The "morph" Option lets your image stretch by length of given string and font-definition.
+Or you just create an image using GD::Simple and pass it to the view.
 
 =head1 SYNOPSIS
 
@@ -22,27 +24,30 @@ You can set $c->stash->{Text2Image} to several options that will define your out
 
  script/myapp_create.pl view Image::Text2Image Image::Text2Image
 
-=item text2image example:
+=item Text2Image example:
 
   Your controller:
 
 	sub Text2Image :Local {
 		my ( $self, $c) = @_;
 		
-		$c->stash->{Text2Image}->{x} = 100;
-		$c->stash->{Text2Image}->{y} = 45;
-		$c->stash->{Text2Image}->{string} ||= 'Done with Catalyst::View::Image::Text2Image';
-		$c->stash->{Text2Image}->{morph} = 1; # Should the x value be adapted when text is too long?
-		$c->stash->{Text2Image}->{font} = 'Times New Roman';
+		$c->stash->{Text2Image}->{string} 	||= 'Done with Catalyst::View::Image::Text2Image';
+		$c->stash->{Text2Image}->{x} 		= 10; # doesn't matter in this case, might be just 1
+		$c->stash->{Text2Image}->{y} 		= 45;
+		$c->stash->{Text2Image}->{font} 	= 'Times New Roman';
 		$c->stash->{Text2Image}->{fontsize} = '15';
-		$c->stash->{Text2Image}->{bgcolor} = 'black';
-		$c->stash->{Text2Image}->{fgcolor} = 'green';
-		$c->stash->{Text2Image}->{moveTo} = [0,45];
-		$c->stash->{Text2Image}->{angle} = -5;
+		$c->stash->{Text2Image}->{bgcolor} 	= 'black';
+		$c->stash->{Text2Image}->{fgcolor} 	= 'green';
+		$c->stash->{Text2Image}->{moveTo} 	= [0,44];
+		$c->stash->{Text2Image}->{angle} 	= -5;
+		$c->stash->{Text2Image}->{morph} 	= 1; ### Should the x value be adapted when text is too long?
+		$c->stash->{Text2Image}->{clear} 	= 1; ### define "clear" to make canvas-color = bgcolor !!
+		$c->stash->{Text2Image}->{transparent} 	= 'white'; # set color to become transparent
+		
 		$c->detach('View::Image::Text2Image');
 	}
 	
-=item example to process a pre-created image with GD::Simple:
+=item Example to process a pre-created image with GD::Simple:
 
   (this leaves you all the possibilities of GD::Simple)
 
@@ -73,13 +78,13 @@ The view is controlled by setting the following values in the stash:
 
 =item $c->stash->{Text2Image}->{img}
 
-(optional) Can contain a GD::Image object. If set, the view will not create an image but try to process this object. 
-If not set, you need at least to provide x,y,string, font and fontsize.
+(optional) Can contain a GD::Image object. If set, the view will not create an image but try to process/render this object. 
+If not set, you need at least to provide "y","string", "font" and "fontsize".
 
 =item $c->stash->{Text2Image}->{x}
 
-The width (in pixels) of the image.
-Mandatory if not $c->stash->{Text2Image}->{img}
+The width (in pixels) of the image. Might be altered if option "morph" is set.
+Only considered if not $c->stash->{Text2Image}->{img}.
 
 =item $c->stash->{Text2Image}->{y}
 
@@ -88,14 +93,14 @@ Mandatory if not $c->stash->{Text2Image}->{img}
 
 =item $c->stash->{Text2Image}->{string}
 
-The Text of the image.
-Only one line supported in this early version.
+The Text of the image. Only one line supported in this version.
 Mandatory if not $c->stash->{Text2Image}->{img}
 
 =item $c->stash->{Text2Image}->{morph}
 
-(optional) Bool. Should the width of the image be adapted to the width of $c->stash->{Text2Image}->{string}? Note: The option "angle" isn't working with "morph" in this early version. 
-Only possible if not $c->stash->{Text2Image}->{img}
+(optional) Bool. Should the width of the image be adapted to the width of $c->stash->{Text2Image}->{string}? 
+Note: The option "angle" isn't considered in "morph"-functionality, so your images may become to wide if you use "angle" and "morph" together.
+Only considered if not $c->stash->{Text2Image}->{img}
 
 =item $c->stash->{Text2Image}->{font}
 
@@ -107,21 +112,22 @@ Mandatory if not $c->stash->{Text2Image}->{img}
 The font to use for the text.
 Mandatory if not $c->stash->{Text2Image}->{img}
 
-=item more...
+=item MORE Options...
 
 More GD::Simple Options are just applied by values in the stash.
-Tested in this version are: bgcolor, fgcolor, moveTo, turn and angle. 
-See example for more informations.
+Tested in this version are: bgcolor, fgcolor, moveTo, clear, transparent and angle. 
+Refer to examples for more informations on that.
 
 =back
 
 =head2 Image format
 
-The generated images will always be produced in the PNG-Format in this version.
+The generated images will always be produced in the PNG-Format.
 
 =head1 SEE ALSO
 
 GD::Simple
+Catalyst::View
 
 =head1 AUTHOR
 
@@ -129,7 +135,7 @@ Martin Gillmaier (MG), C<< <gillmaus at cpan.org> >>
 
 =head1 SUPPORT
 
-Critic, requests, commercial support and training for this module is available:
+Critics, requests, commercial support and training for this module is available:
 Contact L<gillmaus at cpan.org> for details.
 
 =head1 COPYRIGHT & LICENSE
@@ -147,13 +153,13 @@ or implied warranties.
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 METHODS
 
 =head2 process
 	
-	main method af the view
+	Main method of the view.
 	
 =cut
 
@@ -162,7 +168,7 @@ sub process {
   
   # Is there an pre-created image already?
   unless (defined($c->stash->{Text2Image}->{'img'})) {
-	  # No? Create one:
+	  # No? Create one using &text2image:
 	  $c->stash->{'Text2Image'}->{'img'} = $self->text2image($c->stash->{'Text2Image'});
   }
   
@@ -176,7 +182,7 @@ sub process {
 
 	Converts a texts into a png-image using GD::Simple;
 	
-	param: Hash-Ref of GD::Simple options (meaning function=>value), for example: 
+	param: Hash-Ref of GD::Simple options (meaning: function=>value), for example: 
 			{
 				### MUST-values:
 				x => 100,
@@ -185,12 +191,14 @@ sub process {
 				fontsize => 18,
 				string => 'Huhu..its me..your pic!',
 				### OPTIONAL Values:
-				morph => 1, # optional, will adapt x to string-width 
+				morph => 1, 	# optional, will adapt x to string-width 
+				clear => '1', 	# to set canvas-color=bgcolor. 
+				transparent => 'white' # make background transparent
 				### other optional, GD::Simple Values:
 				fgcolor => 'black',
-				bgcolor => 'green',
-				turn => 90,
-				moveTo => [0,25]
+				bgcolor => 'white',
+				angle => -5,
+				moveTo => [0,20] # Move pencil 
 			}
 			
 	return: GD image-Object
@@ -201,9 +209,11 @@ sub text2image {
   my %opts = %{$options};
   
   # check: 
-  return 'x value missing' if $opts{y}<1; # height not negative
-  return 'y value missing' if $opts{x}<1; # width not negative
-  return 'string value missing' unless $opts{string}<1; # width not negative
+  $opts{x} = 1 if $opts{x}<1; # height not 0 or negative
+  return 'y value missing' if $opts{y}<1; # width not 0 or negative
+  return 'string value missing' unless $opts{string}; # text-width not 0
+  return 'font value missing' unless $opts{font}; # font not undef
+  return 'fontsize value missing' if $opts{fontsize} <1; # fontsize not 0
   
   # Create image:
   my $img = GD::Simple->new($opts{x}, $opts{y});
@@ -219,20 +229,30 @@ sub text2image {
   # Try to apply all other options:
   eval {
 	foreach my $opt (keys %opts) {
-	  next if ($opt eq 'x' || $opt eq 'y' || $opt eq 'morph' || $opt eq 'string');
+	  next if ($opt eq 'x' || $opt eq 'y' || $opt eq 'morph' || $opt eq 'string' || $opt eq 'clear');
+	  
 	  if (ref($opts{$opt}) eq 'ARRAY') {
+		# Array-Parameters
 		$img->$opt( @{$opts{$opt}} );
+		
+	  } elsif (!$opts{$opt}) {
+		# No Params:
+		$img->$opt();
+		
 	  } else {
+		# One Parameter:
 		$img->$opt( $opts{$opt} );
 	  }
 	}
   };
   return $@ if ($@);
   
+  # Change background color?
+  $img->clear if (exists $opts{clear});
+  
   # Now apply string:
   $img->string($opts{string});
   
-  #~ die ($img);
   return $img;
 }
 
